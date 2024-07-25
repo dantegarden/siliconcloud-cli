ee8377af6eb6ee42a4df0c1dda8e5e0f9cf9abf5
dantegarden <761043617@qq.com>
2024-07-25 15:18:14 +0800

    fix: github workflow

--- tools/download_assets.sh
+++ tools/download_assets.sh
@@ -15,9 +15,9 @@ LIST=(
 for filename in "${LIST[@]}"
 do
     ASSET_ID=`curl -X GET -H "Content-Type: application/json" -H "Authorization: token $GITHUB_TOKEN" -H "Accept: application/vnd.github.v3.raw" $URL | jq ".[0].assets | map(select(.name == \"$filename\"))[0] | .id"`
-    echo $ASSET_ID
+    echo ASSET_ID=$ASSET_ID
     wget -q --auth-no-challenge --header='Accept:application/octet-stream' https://$GITHUB_TOKEN:@api.github.com/repos/$REPO/releases/assets/$ASSET_ID  -O $filename
-    ls -l
+    ls -l siliconcloud-cli*
     shasum -a 256 "$filename" >> SHASUMS256.txt
 done
 
